f69092a1150ecb329161adcd69bc3b0743570eb8
AminaAsif-git-hub <145750983+Muhammad-Zubair00@users.noreply.github.com>
2024-12-02 20:00:15 +0500

    sumPositive

--- sum-pos.asm
+++ sum-pos.asm
@@ -1,18 +1,35 @@
 include irvine32.inc
 .data
-
-; array and other variables declaration here
-
+array SWORD -3, -6, -1, -10, 10, 30, 40, 4
+msg1 db "Sum of positive numbers is: ", 0
+msg2 db "Count of positive numbers is: ", 0
+sum dword 0
+count dword 0
 .code
 main proc
-
-
-;code here
-
-
-
+call clrscr
+mov esi, offset array
+mov ecx, LENGTHOF array
+process_loop:
+   mov ax, word ptr [esi]
+   test ax, 8000h
+   jnz skip_positive
+   movsx eax, ax
+   add sum, eax
+   inc count
+skip_positive:
+   add esi, 2
+   loop process_loop
+mov edx, offset msg1
+call writestring
+mov eax, sum
+call writeint
+call crlf
+mov edx, offset msg2
+call writestring
+mov eax, count
+call writeint
+call crlf
 exit
 main endp
-
-
 end main
